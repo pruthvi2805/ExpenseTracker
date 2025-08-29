1deb39677739c20f6e52e5acc717b58098715e61
Pruthvi <kautikwarpruthvi@gmail.com>
2025-08-29 22:25:42 +0200

    chore: remove data migration hook and file

--- pf-dashboard/src/main.jsx
+++ pf-dashboard/src/main.jsx
@@ -5,18 +5,12 @@ import { BrowserRouter } from 'react-router-dom'
 import '@fontsource/inter/latin-400.css'
 import '@fontsource/inter/latin-600.css'
 import './index.css'
-import { runMigrations } from './lib/migrate.js'
 import App from './App.jsx'
 
-async function boot(){
-  await runMigrations()
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
-  )
-}
-
-boot()
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+)
