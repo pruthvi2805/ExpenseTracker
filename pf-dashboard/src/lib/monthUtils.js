51aac2cf98efb009ff57cb84d240474412e0c097
Pruthvi <kautikwarpruthvi@gmail.com>
2025-08-31 20:08:54 +0200

    refactor: Use constants for date formats in monthUtils.js

--- pf-dashboard/src/lib/monthUtils.js
+++ pf-dashboard/src/lib/monthUtils.js
@@ -1,16 +1,17 @@
 import { format } from 'date-fns'
+import { DATE_FORMATS } from './constants.js'
 
 export function nextMonthKey(monthKey) {
   const [y, m] = monthKey.split('-').map(Number)
   const d = new Date(y, m - 1, 1)
   d.setMonth(d.getMonth() + 1)
-  return format(d, 'yyyy-MM')
+  return format(d, DATE_FORMATS.MONTH_KEY)
 }
 
 export function prevMonthKey(monthKey) {
   const [y, m] = monthKey.split('-').map(Number)
   const d = new Date(y, m - 1, 1)
   d.setMonth(d.getMonth() - 1)
-  return format(d, 'yyyy-MM')
+  return format(d, DATE_FORMATS.MONTH_KEY)
 }
 
